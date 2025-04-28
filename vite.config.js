d3720fa7a7332c5e4c85747ab8d954fdad589992
Ahrin <hinatairene99@gmail.com>
2025-04-29 03:03:50 +0800

    base homepage updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/robofriendsVite-v2/'
+  base: '/robofriendsVite-Deploy/'
 })
